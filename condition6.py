587a3305143f872d98e889d2f943f437b3c9315d
riteshpatil5601 <riteshpatil56019210@gmail.com>
2026-07-22 04:41:20 +0530

    Day 5

---
+++ condition6.py
@@ -0,0 +1,13 @@
+a=int(input("Enter marks:"))
+if a>=90 and a<=100:
+    print("Ex grade")
+elif a>=80 and a<90:
+    print("A grade")
+elif a>=70 and a<80:
+    print("B grade")
+elif a>=60 and a<70:
+    print("C grade")
+elif a>=50 and a<60:
+    print("D grade")
+else:
+    print("fail")
